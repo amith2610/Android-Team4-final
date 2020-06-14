156c0a3c328974c644df5219fa34caaa291486d1
amith2610 <amith2610@gmail.com>
2020-06-14 18:47:16 +0530

    updated activitymain2

--- app/src/main/java/com/example/sportsapp/MainActivity2.java
+++ app/src/main/java/com/example/sportsapp/MainActivity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -35,13 +36,21 @@ public class MainActivity2 extends AppCompatActivity {
         String c="count";
         LinearLayout newResourse=new LinearLayout(this);
         newResourse.setOrientation(LinearLayout.VERTICAL);
+        newResourse.setBackgroundColor(getResources().getColor(R.color.layoutbg));
+        newResourse.setPadding(18,32,18,32);
         TextView tvResourse= new TextView(this);
         TextView tvCount= new TextView(this);
+        Space space=new Space(this);
         tvResourse.setText(r);
         tvCount.setText(c);
         newResourse.addView(tvResourse);
         newResourse.addView(tvCount);
         list.addView(newResourse);
+        list.addView(space,0,60);
+        list.setPadding(50,0,50,);
+
+
+
 
     }
 
