8de59a5a8d50ef6888f4427160a79d4e660a44dd
amith2610 <amith2610@gmail.com>
2020-06-14 17:09:19 +0530

    added list

--- app/src/main/java/com/example/sportsapp/MainActivity2.java
+++ app/src/main/java/com/example/sportsapp/MainActivity2.java
@@ -3,6 +3,11 @@ package com.example.sportsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        FloatingActionButton addbtn;
+
+        addbtn=(FloatingActionButton) findViewById(R.id.addbtn);
+         final LinearLayout list;
+         list=(LinearLayout) findViewById(R.id.list);
+
+         addbtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 add(list);
+             }
+         });
+
     }
+
+    private void add(LinearLayout list) {
+        String r="Resourse";
+        String c="count";
+        LinearLayout newResourse=new LinearLayout(this);
+        newResourse.setOrientation(LinearLayout.VERTICAL);
+        TextView tvResourse= new TextView(this);
+        TextView tvCount= new TextView(this);
+        tvResourse.setText(r);
+        tvCount.setText(c);
+        newResourse.addView(tvResourse);
+        newResourse.addView(tvCount);
+        list.addView(newResourse);
+
+    }
+
 }
